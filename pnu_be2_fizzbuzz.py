b7aa2cb1e3d7f9c6135ecf93210bd0bc20a35310
정진택 <87135698+jjt4515@users.noreply.github.com>
2024-06-04 20:30:42 +0900

    ifdajlks

--- pnu_be2_fizzbuzz.py
+++ pnu_be2_fizzbuzz.py
@@ -1,6 +1,12 @@
 def fizzbuzz(n):
     if(n%3==0 and n%5 ==0):
         print("fizzbuzz")
+    elif n % 3 == 0 : 
+        print('Fizz')
+    elif n % 5 == 0 :
+        print('Buzz')
+    else :
+        print(n)
 
 for i in range(1, 101):
     fizzbuzz(i)
